f79554a2943bf4218a6de65861eaac4ccca1dedd
Michael Solimini <mjsolimini@gmail.com>
2017-06-02 23:10:04 -0400

    first commit

---
+++ Whealth-and-Co/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Whealth-and-Co
+//
+//  Created by Michael Solimini on 3/15/17.
+//  Copyright © 2017 Alpha Dev. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Container: UIView!
+    var WebView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        WebView = WKWebView()
+        Container.addSubview(WebView)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let frame = CGRect(x: 0, y: 0, width: Container.bounds.width, height: Container.bounds.height)
+        WebView.frame = frame
+        loadRequest(urlStr: "http://www.whealthandco.com/")
+    }
+    
+    func loadRequest(urlStr: String!) {
+        let url = URL(string: urlStr)!
+        let request = URLRequest(url: url)
+        WebView.load(request)
+    }
+
+}
+
---
+++ Whealth-and-CoTests/Whealth_and_CoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Whealth_and_CoTests.swift
+//  Whealth-and-CoTests
+//
+//  Created by Michael Solimini on 3/15/17.
+//  Copyright © 2017 Alpha Dev. All rights reserved.
+//
+
+import XCTest
+@testable import Whealth_and_Co
+
+class Whealth_and_CoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
